65cc07eaf323f4f0e0be9c2f63616527ca7eec3a
Mintical <mashoobee@gmail.com>
2018-06-11 18:50:38 -0700

    Update Main.php

--- src/Sycrux/Main.php
+++ src/Sycrux/Main.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace Sycrux;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\server;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as t;
+
+class Main extends Pluginbase implements Listener {
+  
+}
